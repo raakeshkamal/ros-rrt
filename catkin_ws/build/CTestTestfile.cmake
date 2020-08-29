1d72db9a84097646f24b73f9bd62910a71ed5673
Raakesh kamal <raakeshkamal@gmail.com>
2020-08-29 23:07:50 +0530

    vanilla rrt done : rrtstar to be added

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raakesh/Documents/ros-rrt/catkin_ws/src
-# Build directory: /home/raakesh/Documents/ros-rrt/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rrt")
